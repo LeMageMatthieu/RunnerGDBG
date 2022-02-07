384a3d6866f8e2a9cd0236f04c0755a2d0f23b4f
UnityMakerLeoVINCENT <leovincent859@gmail.com>
2022-02-07 16:55:24 +0100

    update

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float moveSpeed = 3;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
     }
 }
+
+
+
+
